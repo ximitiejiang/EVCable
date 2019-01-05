b45f656f6eaf1e19f1b824abf065b4c8ab6eea66
ximitiejiang <ximitiejiang@163.com>
2019-01-05 15:17:49 +0800

    creat basic classes

---
+++ EVCable/models/drive_cycle/drive_cycle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  5 15:13:03 2019
+
+@author: suliang
+"""
+
+class DriveCycle():
+    
+    def __init__(self):
+        pass
+    
---
+++ EVCable/models/motor/motor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  5 15:15:52 2019
+
+@author: suliang
+"""
+
+class Motor():
+    
+    def __init__(self):
+        pass
