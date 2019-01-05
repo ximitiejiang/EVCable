b5e8f837cedd3c4de305601dd6b8eba5e1661838
ximitiejiang <ximitiejiang@163.com>
2019-01-05 15:08:46 +0800

    update

--- EVCable/configs/cfg_ev_nissan_leaf.py
+++ EVCable/configs/cfg_ev_nissan_leaf.py
@@ -7,8 +7,8 @@ Created on Fri Jan  4 11:15:26 2019
 """
 
 # vehicle
-Vehicle={
-    Type = 'EV',
+vehicle=dict(
+    type = 'EV',
     vehicle_name = 'NissanLeaf',
     weight = 1490,
     height = 1.5,
@@ -16,5 +16,19 @@ Vehicle={
     width = 2.6,
     cd = 0.23,
     wheel_radia = 0.85
-    }
+    )
+
+battery=dict(
+    type='',
+    capacity=45,
+    )
+
+wheel = dict(
+    radium=0.85)
+
+connector=dict(
+    type='AK',
+    size=35)
+
+cable=dict(size=35)
 # 
---
+++ Example/main_ev_nissan_leaf.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  4 22:58:22 2019
+
+@author: suliang
+"""
+
+def main():
+    
+    pass
+
+
+if __name__=='__main__':
+    main()
